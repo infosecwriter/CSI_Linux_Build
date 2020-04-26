85c41fc2cd27f98908aab31f37468536a531fc1c
Steve Miller <me@r15cookie.com>
2020-04-26 19:53:12 -0400

    Functional anible

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -eux
 
+# Disable the proxy, if available (and not error out)
+sed -i 's/\(.*Proxy.*\)/# \1/g' /etc/apt/apt.conf || true
+
 # Install Ansible repository.
 apt -y update && apt-get -y upgrade
 apt -y install software-properties-common
-apt-add-repository ppa:ansible/ansible
+apt-add-repository -y ppa:ansible/ansible
 
 # Install Ansible.
 apt -y update
